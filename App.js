1f7bbdb1916ffba1054f3b9c73b143d9f6772a94
Eduardo.06 <eduardosamuelbarbosademoraes@gmail.com>
2024-08-27 17:22:11 -0300

    .

---
+++ App.js
@@ -0,0 +1,70 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Fontisto, MaterialIcons } from '@expo/vector-icons';
+
+import Login from './Screens/Login_tela';
+import Home from './Screens/Home_tela';
+import Localizacao from './Screens/Localizacao';
+import Pagamento from './Screens/Pagamentos';
+
+function HomeTabs() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator
+      initialRouteName='HomeTabs'
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "grey",
+        tabBarActiveBackgroundColor: "transparent",
+        tabBarInactiveBackgroundColor: "transparent",
+        headerStyle: { backgroundColor: "red" },
+        headerTitle: ' '
+      }}
+    >
+      <Tab.Screen
+        name='Localizacao'
+        component={Localizacao}
+        options={{
+          tabBarLabel: 'Localizacao',
+          tabBarIcon: ({ color }) => (<Fontisto name="bus" size={30} color={color} />)
+        }}
+      />
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (<Fontisto name="home" size={30} color={color} />)
+        }}
+      />
+      <Tab.Screen
+        name='Pagamento'
+        component={Pagamento}
+        options={{
+          tabBarLabel: 'Pagamento',
+          tabBarIcon: ({ color }) => (<MaterialIcons name="attach-money" size={38} color={color} />)
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "white" },
+          headerTintColor: "red",
+          headerTitleAlign: "center"
+        }}
+      >
+        <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name='Home' component={HomeTabs} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Screens/Login_tela.js
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, Image, TouchableOpacity, Text, View, Animated } from 'react-native';
+import { FontAwesome, Feather } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BlurView } from 'expo-blur';
+
+export default function Login({ navigation }) {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [buttonScale] = useState(new Animated.Value(1));
+
+  const handleLogin = () => {
+    Animated.sequence([
+      Animated.timing(buttonScale, {
+        toValue: 0.95,
+        duration: 150,
+        useNativeDriver: true,
+      }),
+      Animated.timing(buttonScale, {
+        toValue: 1,
+        duration: 150,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      if (login === 'dudu' && password === '123') {
+        navigation.navigate('Home');
+      } else {
+        alert('Credenciais inválidas. Tente novamente.');
+      }
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['black', '#EAEAEA','#EAEAEA','#EAEAEA', 'black']}
+        style={styles.background}
+      />
+      <BlurView intensity={70} style={styles.blurContainer}>
+        <Image
+          style={styles.imagem}
+          source={require('../ft/MAVERIK.png')}
+        />
+        <Text style={styles.txt}>Transformando</Text>
+        <Text style={styles.txt1}>sonhos em destinos!</Text>
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <Feather name="user" size={24} color="#6D6D6D" />
+            <TextInput
+              style={styles.input}
+              placeholder="Login"
+              placeholderTextColor="#6D6D6D"
+              value={login}
+              onChangeText={setLogin}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <FontAwesome name="lock" size={24} color="#6D6D6D" />
+            <TextInput
+              style={styles.input}
+              placeholder="Senha"
+              placeholderTextColor="#6D6D6D"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+          </View>
+          <Animated.View style={{ transform: [{ scale: buttonScale }] }}>
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </BlurView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  blurContainer: {
+    width: '90%',
+    padding: 20,
+    borderRadius: 25,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+    shadowRadius: 15,
+    elevation: 5,
+  },
+  imagem: {
+    height: 120,
+    width: 120,
+    marginBottom: 20,
+    borderRadius: 60,
+    borderColor: '#EAEAEA',
+    borderWidth: 2,
+  },
+  txt: {
+    fontSize: 26,
+    color: '#333',
+    fontWeight: '700',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  txt1: {
+    fontSize: 20,
+    color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    marginVertical: 10,
+    width: '100%',
+    height: 50,
+    backgroundColor: '#F5F5F5',
+    borderColor: '#E0E0E0',
+    borderWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 1,
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+    color: '#333',
+    fontSize: 16,
+  },
+  button: {
+    width: '100%',
+    height: 42,
+    borderRadius: 25,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    borderWidth: 0, 
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 4,
+    paddingHorizontal: 30, // Aumenta o tamanho no eixo X
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  form: {
+    width: '100%',
+    alignItems: 'center',
+  },
+});
