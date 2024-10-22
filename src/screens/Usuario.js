252ad0908d92bb64a6651672f2c81f771394be99
Eduardo.06 <eduardosamuelbarbosademoraes@gmail.com>
2024-10-22 17:18:52 -0300

    .

--- src/screens/Usuario.js
+++ src/screens/Usuario.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
-
+import styles from '../styles/Usuario.module.css'; // Importa o módulo CSS
 
 function Usuario() {
-
-    //inputs cadastro
+    // Inputs cadastro
     const [nome, setNome] = useState('');
     const [horarioida, setHorarioida] = useState('');
     const [horariovolta, setHorariovolta] = useState('');
@@ -16,8 +15,9 @@ function Usuario() {
     const [cpf, setCpf] = useState('');
     const [telefone, setTelefone] = useState('');
     const [email, setEmail] = useState('');
+    const [foto, setFoto] = useState(null); // Campo para foto (inicialmente nulo)
 
-    //inputs editar
+    // Inputs editar
     const [id, setId] = useState('');
     const [alterLogin, setAlterLogin] = useState('');
     const [alterSenha, setAlterSenha] = useState('');
@@ -30,17 +30,18 @@ function Usuario() {
     const [alterCpf, setAlterCpf] = useState('');
     const [alterTelefone, setAlterTelefone] = useState('');
     const [alterEmail, setAlterEmail] = useState('');
+    const [alterFoto, setAlterFoto] = useState(null); // Campo para foto (inicialmente nulo)
 
-    //pesquisa
+    // Pesquisa
     const [get, setGet] = useState([]);
     const [pesquisa, setPesquisa] = useState('');
     const [ultimoId, setUltimoId] = useState('');
 
-    //modal
+    // Modal
     const [open, setOpen] = React.useState(false);
 
     function handleOpen(usuario) {
-        setId(usuario.id)
+        setId(usuario.id);
         setAlterNome(usuario.nome);
         setAlterHorarioida(usuario.horarioida);
         setAlterHorariovolta(usuario.horariovolta);
@@ -52,187 +53,199 @@ function Usuario() {
         setAlterEmail(usuario.email);
         setAlterLogin(usuario.login);
         setAlterSenha(usuario.senha);
+        setAlterFoto(usuario.foto); // Carrega a foto no modal
         setOpen(true);
     }
 
     const handleClose = () => setOpen(false);
 
     function postUsuarios() {
-        if (nome !== '' && horarioida !== '' && horariovolta !== '' && endereco !== '' && bairro !== '' && cidade !== '' && cpf !== '' && telefone !== '' && email !== '') {
+        if (nome && horarioida && horariovolta && endereco && bairro && cidade && cpf && telefone && email) {
             try {
-                const login = nome + '.' + (ultimoId + 1) //utilizar split para pegar apenas o primeiro nome (será nome completo)
-                const senha = cpf.substring(6, 0)
+                const login = nome + '.' + (ultimoId + 1);
+                const senha = cpf.substring(6, 0);
+                const formData = new FormData(); // Cria um FormData
 
-                axios.post('http://localhost:3000/usuario',
-                    {
-                        nome: nome,
-                        horarioida: horarioida,
-                        horariovolta: horariovolta,
-                        endereco: endereco,
-                        bairro: bairro,
-                        cidade: cidade,
-                        login: login,
-                        senha: senha,
-                        cpf: cpf,
-                        telefone: telefone,
-                        email: email
-                    },
-                )
-                alert('Usuario cadastrado')
-                setNome('')
-                setHorarioida('')
-                setHorariovolta('')
-                setEndereco('')
-                setBairro('')
-                setCidade('')
-                setCpf('')
-                setTelefone('')
-                setEmail('')
-                getUsuarios()
-            }
-            catch (error) {
+                // Adiciona os campos ao FormData
+                formData.append('nome', nome);
+                formData.append('horarioida', horarioida);
+                formData.append('horariovolta', horariovolta);
+                formData.append('endereco', endereco);
+                formData.append('bairro', bairro);
+                formData.append('cidade', cidade);
+                formData.append('login', login);
+                formData.append('senha', senha);
+                formData.append('cpf', cpf);
+                formData.append('telefone', telefone);
+                formData.append('email', email);
+                formData.append('foto', foto); // Adiciona o arquivo da foto
+
+                axios.post('http://localhost:3000/usuario', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+
+                alert('Usuário cadastrado');
+                resetFields();
+                getUsuarios();
+            } catch (error) {
                 console.log(error);
-                alert('Erro ao cadastrar (dados inválidos)')
+                alert('Erro ao cadastrar (dados inválidos)');
             }
+        } else {
+            alert('Insira dados nos campos em branco');
         }
-        else {
-            alert('Insira dados nos campos em branco')
-        }
+    }
+
+    function resetFields() {
+        setNome('');
+        setHorarioida('');
+        setHorariovolta('');
+        setEndereco('');
+        setBairro('');
+        setCidade('');
+        setCpf('');
+        setTelefone('');
+        setEmail('');
+        setFoto(null); // Reseta o campo foto
     }
 
     async function getUsuarios() {
         try {
             const response = await axios.get('http://localhost:3000/usuario');
             const usuarios = response.data.usuarios;
-            
+
             if (usuarios && usuarios.length > 0) {
                 setGet(usuarios);
-                console.log(usuarios);
-
                 const ids = usuarios.map(usuario => usuario.id);
-                const ultimoIds = ids.at(-1);
-                setUltimoId(ultimoIds);
-                console.log('ultimo id: ', ultimoIds);
+                setUltimoId(ids.at(-1));
             } else {
                 setGet([]);
                 setUltimoId('');
-                console.log('Nenhum usuario encontrado');
+                console.log('Nenhum usuário encontrado');
             }
         } catch (error) {
-            console.log('Erro ao obter usuarios:', error);
+            console.log('Erro ao obter usuários:', error);
         }
     }
 
     function putUsuarios() {
-        if (alterNome !== '' && alterHorarioida !== '' && alterHorariovolta !== '' && alterEndereco !== '' && alterBairro !== '' && alterCidade !== '' && alterCpf !== '' && alterTelefone !== '' && alterEmail !== '' && alterLogin !== '' && alterSenha !== '') {
+        if (alterNome && alterHorarioida && alterHorariovolta && alterEndereco && alterBairro && alterCidade && alterCpf && alterTelefone && alterEmail && alterLogin && alterSenha) {
             try {
-                axios.put('http://localhost:3000/usuario',
-                    {
-                        id: id,
-                        nome: alterNome,
-                        horarioida: alterHorarioida,
-                        horariovolta: alterHorariovolta,
-                        endereco: alterEndereco,
-                        bairro: alterBairro,
-                        cidade: alterCidade,
-                        login: alterLogin,
-                        senha: alterSenha,
-                        cpf: alterCpf,
-                        telefone: alterTelefone,
-                        email: alterEmail
-                    }
-                )
-                alert('Usuario alterado')
-            }
-            catch (error) {
+                axios.put('http://localhost:3000/usuario', {
+                    id,
+                    nome: alterNome,
+                    horarioida: alterHorarioida,
+                    horariovolta: alterHorariovolta,
+                    endereco: alterEndereco,
+                    bairro: alterBairro,
+                    cidade: alterCidade,
+                    login: alterLogin,
+                    senha: alterSenha,
+                    cpf: alterCpf,
+                    telefone: alterTelefone,
+                    email: alterEmail,
+                    foto: alterFoto // Atualiza a foto
+                });
+
+                alert('Usuário alterado');
+            } catch (error) {
                 console.log(error);
-                alert('Erro ao editar (dados inválidos)')
+                alert('Erro ao editar (dados inválidos)');
             }
-        }
-        else {
-            alert('Não deixe campos vazios')
+        } else {
+            alert('Não deixe campos vazios');
         }
     }
 
     async function deleteUsuario(id) {
-        await axios.delete('http://localhost:3000/usuario', {
-            data:
-            {
-                id: id
-            }
-        })
-        alert('Usuario deletado');
-        getUsuarios()
+        await axios.delete('http://localhost:3000/usuario', { data: { id } });
+        alert('Usuário deletado');
+        getUsuarios();
     }
 
     useEffect(() => {
         getUsuarios();
     }, []);
 
-
     const filtrar = pesquisa.length > 0
         ? get.filter(filtro => filtro.nome.toLowerCase().includes(pesquisa.toLowerCase()))
         : [];
 
     return (
-        <div>
+        <div className={styles.container}>
+            <h1>Cadastro de Usuário</h1>
+            <input className={styles.input} type='text' value={nome} placeholder='Nome' onChange={event => setNome(event.target.value)} />
+            <input className={styles.input} type='text' value={horarioida} placeholder='Horário Ida' onChange={event => setHorarioida(event.target.value)} />
+            <input className={styles.input} type='text' value={horariovolta} placeholder='Horário Volta' onChange={event => setHorariovolta(event.target.value)} />
+            <input className={styles.input} type='text' value={endereco} placeholder='Endereço' onChange={event => setEndereco(event.target.value)} />
+            <input className={styles.input} type='text' value={bairro} placeholder='Bairro' onChange={event => setBairro(event.target.value)} />
+            <input className={styles.input} type='text' value={cidade} placeholder='Cidade' onChange={event => setCidade(event.target.value)} />
+            <input className={styles.input} type='text' value={cpf} placeholder='CPF' onChange={event => setCpf(event.target.value)} />
+            <input className={styles.input} type='text' value={telefone} placeholder='Telefone' onChange={event => setTelefone(event.target.value)} />
+            <input className={styles.input} type='text' value={email} placeholder='Email' onChange={event => setEmail(event.target.value)} />
+            <input 
+                className={styles.input} 
+                type='file' 
+                accept="image/*" 
+                onChange={event => {
+                    const file = event.target.files[0];
+                    if (file) {
+                        setFoto(file); // Armazena o arquivo
+                    }
+                }} 
+            /> {/* Campo para escolher arquivo da máquina */}
+            <button onClick={postUsuarios}>Cadastrar</button>
+
             <div>
-                <h1>Cadastro usuário</h1>
-                <input type='text' value={nome} placeholder='nome' onChange={event => setNome(event.target.value)} />
-                <input type='text' value={horarioida} placeholder='horario ida' onChange={event => setHorarioida(event.target.value)} />
-                <input type='text' value={horariovolta} placeholder='horario volta' onChange={event => setHorariovolta(event.target.value)} />
-                <input type='text' value={endereco} placeholder='endereco' onChange={event => setEndereco(event.target.value)} />
-                <input type='text' value={bairro} placeholder='bairro' onChange={event => setBairro(event.target.value)} />
-                <input type='text' value={cidade} placeholder='cidade' onChange={event => setCidade(event.target.value)} />
-                <input type='text' value={cpf} placeholder='cpf' onChange={event => setCpf(event.target.value)} />
-                <input type='text' value={telefone} placeholder='telefone' onChange={event => setTelefone(event.target.value)} />
-                <input type='text' value={email} placeholder='email' onChange={event => setEmail(event.target.value)} />
-                <button onClick={postUsuarios}>
-                    cadastrar
-                </button>
+                <h1>Pesquisar Usuário</h1>
+                <input className={styles.input} type='text' placeholder='Pesquisar' onChange={event => setPesquisa(event.target.value)} />
             </div>
-            <div>
-                <h1>Pesquisar usuario</h1>
-                <input type='text' name='pesquisa' value={pesquisa} placeholder='pesquisar' onChange={event => setPesquisa(event.target.value)} />
-                <div>
-                    {pesquisa.length > 0 ? (
-                        filtrar.map((filtro) => (
-                            <div key={filtro.id}>
-                                {filtro.nome}
-                                <button onClick={() => handleOpen(filtro)}>alterar</button>
-                                <button onClick={() => deleteUsuario(filtro.id)}>deletar</button>
-                            </div>
-                        ))
-                    ) : (
-                        get.map((usuario) => (
-                            <div key={usuario.id}>
-                                {usuario.nome}
-                                <button onClick={() => handleOpen(usuario)}>alterar</button>
-                                <button onClick={() => deleteUsuario(usuario.id)}>deletar</button>
-                                <Modal
-                                    open={open}
-                                    onClose={handleClose}
-                                >
-                                    <Box>
-                                        <input type='text' value={alterNome} placeholder='nome' onChange={event => setAlterNome(event.target.value)} />
-                                        <input type='text' value={alterHorarioida} placeholder='horario ida' onChange={event => setAlterHorarioida(event.target.value)} />
-                                        <input type='text' value={alterHorariovolta} placeholder='horario volta' onChange={event => setAlterHorariovolta(event.target.value)} />
-                                        <input type='text' value={alterEndereco} placeholder='endereco' onChange={event => setAlterEndereco(event.target.value)} />
-                                        <input type='text' value={alterBairro} placeholder='bairro' onChange={event => setAlterBairro(event.target.value)} />
-                                        <input type='text' value={alterCidade} placeholder='cidade' onChange={event => setAlterCidade(event.target.value)} />
-                                        <input type='text' value={alterCpf} placeholder='cpf' onChange={event => setAlterCpf(event.target.value)} />
-                                        <input type='text' value={alterTelefone} placeholder='telefone' onChange={event => setAlterTelefone(event.target.value)} />
-                                        <input type='text' value={alterEmail} placeholder='email' onChange={event => setAlterEmail(event.target.value)} />
-                                        <input type='text' value={alterLogin} placeholder='email' onChange={event => setAlterLogin(event.target.value)} />
-                                        <input type='text' value={alterSenha} placeholder='email' onChange={event => setAlterSenha(event.target.value)} />
-                                        <button onClick={putUsuarios}>alterar</button>
-                                    </Box>
-                                </Modal>
-                            </div>
-                        ))
-                    )}
+
+            {filtrar.length > 0 ? filtrar.map((usuario) => (
+                <div key={usuario.id} className={styles.usuario}>
+                    <p>{usuario.nome}</p>
+                    <div>
+                        <button onClick={() => handleOpen(usuario)}>Alterar</button>
+                        <button onClick={() => deleteUsuario(usuario.id)}>Deletar</button>
+                    </div>
                 </div>
-            </div>
+            )) : get.map((usuario) => (
+                <div key={usuario.id} className={styles.usuario}>
+                    <p>{usuario.nome}</p>
+                    <div>
+                        <button onClick={() => handleOpen(usuario)}>Alterar</button>
+                        <button onClick={() => deleteUsuario(usuario.id)}>Deletar</button>
+                    </div>
+                </div>
+            ))}
+
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={{ padding: 2 }}>
+                    <h1>Alterar Usuário</h1>
+                    <input type="text" value={alterNome} onChange={event => setAlterNome(event.target.value)} placeholder="Nome" />
+                    <input type="text" value={alterHorarioida} onChange={event => setAlterHorarioida(event.target.value)} placeholder="Horário Ida" />
+                    <input type="text" value={alterHorariovolta} onChange={event => setAlterHorariovolta(event.target.value)} placeholder="Horário Volta" />
+                    <input type="text" value={alterEndereco} onChange={event => setAlterEndereco(event.target.value)} placeholder="Endereço" />
+                    <input type="text" value={alterBairro} onChange={event => setAlterBairro(event.target.value)} placeholder="Bairro" />
+                    <input type="text" value={alterCidade} onChange={event => setAlterCidade(event.target.value)} placeholder="Cidade" />
+                    <input type="text" value={alterCpf} onChange={event => setAlterCpf(event.target.value)} placeholder="CPF" />
+                    <input type="text" value={alterTelefone} onChange={event => setAlterTelefone(event.target.value)} placeholder="Telefone" />
+                    <input type="text" value={alterEmail} onChange={event => setAlterEmail(event.target.value)} placeholder="Email" />
+                    <input 
+                        type="file" 
+                        accept="image/*" 
+                        onChange={event => {
+                            const file = event.target.files[0];
+                            if (file) {
+                                setAlterFoto(file); // Armazena o arquivo alterado
+                            }
+                        }} 
+                    /> {/* Campo para escolher arquivo da máquina */}
+                    <button onClick={putUsuarios}>Alterar</button>
+                </Box>
+            </Modal>
         </div>
     );
 }
