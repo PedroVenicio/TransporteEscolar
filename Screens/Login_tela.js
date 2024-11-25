a5acf6c83bacd6fdefcedc26ecd36efa4faf87da
EduardoSBM <eduardosamuelbarbosademoraes@gmail.com>
2024-11-25 16:03:33 -0300

    tela de login finalizada

--- Screens/Login_tela.js
+++ Screens/Login_tela.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { TextInput, StyleSheet, Image, TouchableOpacity, Text, View, Animated } from 'react-native';
-import { FontAwesome, Feather } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
-import { BlurView } from 'expo-blur';
+import { TextInput, StyleSheet, TouchableOpacity, Text, View, Animated } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { jwtDecode } from 'jwt-decode';
@@ -26,76 +23,64 @@ export default function Login({ navigation }) {
       }),
     ]).start(async () => {
       try {
-        const response = await axios.post('http://192.168.3.37:3000/login', //casa: 192.168.0.223 | satc: 10.119.0.19
-          {
-            login: login,
-            senha: password
-          });
+        const response = await axios.post('http://192.168.3.37:3000/login', {
+          login: login,
+          senha: password,
+        });
 
         const { access_token } = response.data;
 
-        await AsyncStorage.setItem('token', access_token)
+        await AsyncStorage.setItem('token', access_token);
 
-        const decodedToken = jwtDecode(access_token)
+        const decodedToken = jwtDecode(access_token);
         if (decodedToken.adm === true && decodedToken.motorista === false) {
-          navigation.navigate('HomeAdm')
+          navigation.navigate('HomeAdm');
         }
         if (decodedToken.adm === false && decodedToken.motorista === false) {
-          navigation.navigate('Home')
+          navigation.navigate('Home');
         }
-        if (decodedToken.motorista === true && decodedToken.adm === false){
-          navigation.navigate('HomeMotorista')
+        if (decodedToken.motorista === true && decodedToken.adm === false) {
+          navigation.navigate('HomeMotorista');
         }
-      }
-      catch (error) {
-        console.log('Erro ao logar: ', error)
-        alert('Falha no login, tente novamente mais tarde.')
+      } catch (error) {
+        console.log('Erro ao logar: ', error);
+        alert('Falha no login, tente novamente mais tarde.');
       }
     });
   };
 
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={['', '#EAEAEA', '#EAEAEA', '#EAEAEA', '']}
-        style={styles.background}
-      />
-      <BlurView intensity={60} style={styles.blurContainer}>
-        <Image
-          style={styles.imagem}
-          source={require('../ft/MAVERIK.png')}
+      <View style={styles.triangle} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Bem-vindo,</Text>
+        <Text style={styles.subtitle}>Faça login para</Text>
+        <Text style={styles.subtitle}>usar o app</Text>
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.label}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#6D6D6D"
+          value={login}
+          onChangeText={setLogin}
         />
-        <Text style={styles.txt}>Transformando</Text>
-        <Text style={styles.txt1}>sonhos em destinos!</Text>
-        <View style={styles.form}>
-          <View style={styles.inputContainer}>
-            <Feather name="user" size={24} color="#6D6D6D" />
-            <TextInput
-              style={styles.input}
-              placeholder="Login"
-              placeholderTextColor="#6D6D6D"
-              value={login}
-              onChangeText={setLogin}
-            />
-          </View>
-          <View style={styles.inputContainer}>
-            <FontAwesome name="lock" size={24} color="#6D6D6D" />
-            <TextInput
-              style={styles.input}
-              placeholder="Senha"
-              placeholderTextColor="#6D6D6D"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-            />
-          </View>
-          <Animated.View style={{ transform: [{ scale: buttonScale }] }}>
-            <TouchableOpacity onPress={handleLogin} style={styles.button}>
-              <Text style={styles.buttonText}>Entrar</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        </View>
-      </BlurView>
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#6D6D6D"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <Animated.View style={{ transform: [{ scale: buttonScale }] }}>
+          <TouchableOpacity onPress={handleLogin} style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
     </View>
   );
 }
@@ -103,88 +88,75 @@ export default function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#3B1D1D',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffff',
   },
-  background: {
-    ...StyleSheet.absoluteFillObject,
+  triangle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderRightWidth: 500,
+    borderTopWidth: 250,
+    borderRightColor: 'transparent',
+    borderTopColor: '#7A1F1F',
   },
-  blurContainer: {
-    width: '90%',
-    padding: 20,
-    borderRadius: 25,
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+  titleContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
   },
-  imagem: {
-    height: 120,
-    width: 120,
-    marginBottom: 20,
-    borderRadius: 60,
-    borderColor: '#ffff',
-    borderWidth: 2,
+  title: {
+    fontSize: 36,
+    color: '#FFF',
+    fontWeight: 'bold',
+    marginTop: 15,
   },
-  txt: {
-    fontSize: 26,
-    color: '#333',
-    fontWeight: '700',
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  txt1: {
-    fontSize: 20,
-    color: '#333',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 10,
-    paddingHorizontal: 15,
-    marginVertical: 10,
-    width: '100%',
-    height: 50,
-    backgroundColor: '#F5F5F5',
-    borderColor: '#E0E0E0',
-    borderWidth: 1,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
-    elevation: 1,
-  },
-  input: {
-    flex: 1,
-    paddingHorizontal: 10,
-    color: '#333',
-    fontSize: 16,
-  },
-  button: {
-    width: '100%',
-    height: 42,
-    borderRadius: 10,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-    borderWidth: 0,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 5 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-    elevation: 4,
-    paddingHorizontal: 30, // Aumenta o tamanho no eixo X
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#FFFFFF',
-    fontWeight: '600',
+  subtitle: {
+    fontSize: 24,
+    color: '#FFF',
+    marginTop: 5,
+    marginBottom: -7,
   },
   form: {
-    width: '100%',
+    width: '90%',
+    padding: 20,
+    backgroundColor: '#transparent',
+    borderRadius: 10,
     alignItems: 'center',
-  },
+    marginTop:120,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    color: '#FFF',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    height: 50,
+    paddingHorizontal: 15,
+    color: '#333',
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  button: {
+    marginTop: 40,
+    height: 50,
+    paddingLeft: 75,
+    paddingRight: 75,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#B90000',
+    fontSize: 22,
+  },
 });
